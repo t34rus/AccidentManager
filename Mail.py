a5b79c0bf7c06937688307804f3c2fd8568007c4
t34rus <t34rus@gmail.com>
2015-03-20 13:27:59 +0300

    почта так и не работает

--- Mail.py
+++ Mail.py
@@ -1,24 +1,32 @@
 import re
 import poplib
 import email
-from email import parser
+
 
 def getmails():
+    import sys
     pop3 = poplib.POP3_SSL('pop.mail.ru')
+    print(pop3.getwelcome())
     pop3.user('noreplysup@mail.ru')
     pop3.pass_('fdnjvfn')
     #Get messages from server:
-    mcount=len(pop3.list()[1])
-    for i in range(1,mcount):
-        msg=pop3.retr(i+1)
-        raw_mail='\n'.join(msg[1])
-        mail=email.message_from_string(raw_mail)
-
-        # subject
-        subject = mail.get('Subject')
-        h = email.header.decode_header(subject)
-        msg = h[0][0].decode(h[0][1]) if h[0][1] else h[0][0]
-        print(msg)
+    pop3.list()
+    (numMsgs, totalSize) = pop3.stat()
+    for i in range(1, numMsgs + 1):
+        response  = pop3.retr(i)
+        # return in format: (response, ['line', ...], octets)
+        raw_message = response[1]
+        for msg in raw_message:
+            if email.message_from_bytes(msg).startswith('Subject'):
+                print ('\t' + msg)
+        str_message = email.message_from_bytes(b'\n'.join(raw_message))
+        print(str_message.get_subject())
+        for part in str_message.walk():
+            print(part.get_content_type())
+            if part.get_content_type() == 'text/plain':
+                body = part.get_payload()
+                #print(body)
+        print('-----------------------')
     pop3.quit()
 
 from Scheduler import *
